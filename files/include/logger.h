f5cf552b5ac211796ee2a61891e2647929cf7d85
AboodXD <aboood40091@gmail.com>
2019-06-05 21:19:44 +0400

    Add Cemu version for v1.3.0 US

--- files/include/logger.h
+++ files/include/logger.h
@@ -1,2 +1,6 @@
 #pragma once
+#ifndef Cemu
 #define log_printf(FMT, ARGS...)  ((void (*)(const char *format, ...))(*(unsigned int *)(DATA_ADDR + 0x20000)))(FMT, ## ARGS);
+#else
+#define log_printf(x, ...)
+#endif
--- files/src/ctors.cpp
+++ files/src/ctors.cpp
@@ -1,6 +1,13 @@
 extern "C" {
     void callCtors() {
-        unsigned int *ctors = (unsigned int *)(DATA_ADDR + 0x20004);
+        unsigned int *ctors;
+
+        #ifdef Cemu
+        ctors = (unsigned int *)(*(unsigned int *)(DATA_ADDR - 4));
+        #else
+        ctors = (unsigned int *)(DATA_ADDR + 0x20004);
+        #endif
+
         unsigned int count = ctors[0];
         for (int i = 0; i < count; i++) {
             ((void (*)())(ctors[i + 1]))();
