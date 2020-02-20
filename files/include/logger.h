a0eff5e25489b2a3936310878d493541cd9d1f26
AboodXD <aboood40091@gmail.com>
2020-02-20 19:38:10 +0400

    Comply with the new CafeLoader version

--- files/include/logger.h
+++ files/include/logger.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifndef Cemu
-#define log_printf(FMT, ARGS...)  ((void (*)(const char *format, ...))(*(unsigned int *)(DATA_ADDR + 0x20000)))(FMT, ## ARGS);
+#define log_printf(FMT, ARGS...)  ((void (*)(const char *format, ...))(*(unsigned int *)(DATA_ADDR - 4)))(FMT, ## ARGS);
 #else
 #define log_printf(x, ...)
 #endif
--- files/src/ctors.cpp
+++ files/src/ctors.cpp
@@ -1,16 +1,14 @@
+typedef void (*InitFunc)();
+extern InitFunc _ctors[];
+
 extern "C" {
     void callCtors() {
-        unsigned int *ctors;
+        static bool initialized = false;
+        if (initialized)
+            return;
 
-        #ifdef Cemu
-        ctors = (unsigned int *)(*(unsigned int *)(DATA_ADDR - 4));
-        #else
-        ctors = (unsigned int *)(DATA_ADDR + 0x20004);
-        #endif
-
-        unsigned int count = ctors[0];
-        for (int i = 0; i < count; i++) {
-            ((void (*)())(ctors[i + 1]))();
-        }
+        initialized = true;
+        for (int i = 0; _ctors[i]; i++)
+            (*_ctors[i])();
     }
 }
